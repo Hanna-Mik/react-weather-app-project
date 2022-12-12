b036ac1215e8209763cc6d71add08dc9dbee369c
Hanna-Mik <110687941+Hanna-Mik@users.noreply.github.com>
2022-12-12 17:03:13 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <footer>
+          This App was created by Hanna Shchepeleva and is{" "}
+          <a href="/">open-sourced on GitHub</a>
+        </footer>
+      </div>
+    </div>
+  );
 }
 
 export default App;

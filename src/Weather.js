0aad1f98fa32e08e0d217bd540887c3e3370a113
Hanna-Mik <110687941+Hanna-Mik@users.noreply.github.com>
2022-12-13 17:39:36 +0200

    added API implementation

--- src/Weather.js
+++ src/Weather.js
@@ -1,54 +1,100 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
-  return (
-    <div className="Weather">
-      <form>
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Enter a city.."
-              className="form-control"
-            />
-          </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-primary w-100
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      ready: true,
+      temperature: response.data.main.temp,
+      city: response.data.name,
+      description: response.data.weather[0].description,
+      wind: response.data.wind.speed,
+      pressure: response.data.main.pressure,
+      humidity: response.data.main.humidity,
+      date: "Monday 09:00",
+      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+    });
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city.."
+                className="form-control"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100
               "
-            />
+              />
+            </div>
           </div>
-        </div>
-      </form>
-      <div className="row mt-4">
-        <div className="col-3 text-end">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            alt="partly cloudy"
-          />{" "}
-          <span className="temperature">2</span>
-          <span className="unit">°C</span>
-        </div>
-        <div className="col-4">
-          <ul className="currentWeatherDetails">
-            <li>Precipitation: 2%</li>
-            <li>Humidity: 76%</li>
-            <li>Wind: 3 m/s</li>
-          </ul>
-        </div>
-        <div className="col-5 placeInfo">
-          <ul>
-            <li>
-              <h1>New York</h1>
-            </li>
-            <li>Monday 09:00</li>
-            <li>Partly cloudy</li>
-          </ul>
+        </form>
+        <div className="row mt-4">
+          <div className="col-4 text-end">
+            <img src={weatherData.iconUrl} alt={weatherData.description} />{" "}
+            <span className="temperature">
+              {Math.round(weatherData.temperature)}
+            </span>
+            <span className="unit">°C</span>
+          </div>
+          <div className="col-4">
+            <ul className="currentWeatherDetails">
+              <li>Pressure: {weatherData.pressure}hPa</li>
+              <li>Humidity: {weatherData.humidity}%</li>
+              <li>Wind: {Math.round(weatherData.wind)} m/s</li>
+            </ul>
+          </div>
+          <div className="col-4 placeInfo">
+            <ul>
+              <li>
+                <h1>{weatherData.city}</h1>
+              </li>
+              <li>{weatherData.date}</li>
+              <li className="text-capitalize">{weatherData.description}</li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let apiKey = "738993d32099f81cb584e637be73ea30";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+    return (
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city.."
+                className="form-control"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100
+              "
+              />
+            </div>
+          </div>
+        </form>
+        <div>The data is loading..</div>
+      </div>
+    );
+  }
 }

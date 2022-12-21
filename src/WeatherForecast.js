1ffd9c74283cfbb58c597759a55f997c09d15c5f
Hanna-Mik <110687941+Hanna-Mik@users.noreply.github.com>
2022-12-21 17:00:24 +0200

    refactored code

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -15,6 +15,15 @@ export default function WeatherForecast(props) {
     setLoaded(false);
   }, [props.coordinates]);
 
+  function load() {
+    let apiKey = "b7otd3ffec1d40c723a936cbb2dda9f5";
+    let lon = props.coordinates.longitude;
+    let lat = props.coordinates.latitude;
+
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${lon}&lat=${lat}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
   if (loaded) {
     return (
       <div className="WeatherForecast">
@@ -34,12 +43,7 @@ export default function WeatherForecast(props) {
       </div>
     );
   } else {
-    let apiKey = "b7otd3ffec1d40c723a936cbb2dda9f5";
-    let lon = props.coordinates.longitude;
-    let lat = props.coordinates.latitude;
-
-    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${lon}&lat=${lat}&key=${apiKey}`;
-    axios.get(apiUrl).then(handleResponse);
+    load();
     return null;
   }
 }

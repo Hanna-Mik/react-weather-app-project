334e1817851f49171c3e4656037c732b8a5c893e
Hanna-Mik <110687941+Hanna-Mik@users.noreply.github.com>
2022-12-21 16:32:03 +0200

    showing temperature in Fahrenheit removed

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,42 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function WeatherTemperature(props) {
-  const [unit, setUnit] = useState("celsius");
-  function showFahrenheit(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-  }
-  function showCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
-  function convertToFahrenheit() {
-    return Math.round(props.celsius * 1.8 + 32);
-  }
-
-  if (unit === "celsius") {
-    return (
-      <span className="WeatherTemperature">
-        <span className="temperature">{Math.round(props.celsius)}</span>
-        <span className="unit">
-          °C |{" "}
-          <a href="/" onClick={showFahrenheit}>
-            °F
-          </a>{" "}
-        </span>
-      </span>
-    );
-  } else {
-    return (
-      <span className="WeatherTemperature">
-        <span className="temperature">{convertToFahrenheit()}</span>
-        <span className="unit">
-          <a href="/" onClick={showCelsius}>
-            °C
-          </a>{" "}
-          | °F
-        </span>
-      </span>
-    );
-  }
+  return (
+    <span className="WeatherTemperature">
+      <span className="temperature">{Math.round(props.celsius)}</span>
+      <span className="unit">°C</span>
+    </span>
+  );
 }

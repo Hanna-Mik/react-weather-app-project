33a179b95e6451895130d0baacdb54a563cf9fcf
Hanna-Mik <110687941+Hanna-Mik@users.noreply.github.com>
2022-12-22 21:03:27 +0200

    responsiveness improved

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -6,7 +6,7 @@ import WeatherTemperature from "./WeatherTemperature";
 export default function WeatherInfo(props) {
   return (
     <div className="WeatherInfo">
-      <div className="row mt-5">
+      <div className="row mt-5 d-none d-md-block">
         <div className="col-4 text-end">
           <WeatherIcon code={props.data.icon} size={52} />
           <WeatherTemperature celsius={props.data.temperature} />
@@ -27,6 +27,30 @@ export default function WeatherInfo(props) {
               <FormattedDate date={props.data.date} />
             </li>
             <li className="text-capitalize">{props.data.description}</li>
+          </ul>
+        </div>
+      </div>
+      <div className="row mt-5 d-block d-md-none">
+        <div className="col-12 text-center placeInfo">
+          <ul>
+            <li>
+              <h1>{props.data.city}</h1>
+            </li>
+            <li>
+              <FormattedDate date={props.data.date} />
+            </li>
+            <li className="text-capitalize">{props.data.description}</li>
+          </ul>
+        </div>
+        <div className="col-12 text-center mt-4">
+          <WeatherIcon code={props.data.icon} size={52} />
+          <WeatherTemperature celsius={props.data.temperature} />
+        </div>
+        <div className="col-12 mt-4">
+          <ul className="currentWeatherDetails text-center">
+            <li>Pressure: {props.data.pressure}hPa</li>
+            <li>Humidity: {props.data.humidity}%</li>
+            <li>Wind: {Math.round(props.data.wind * 10) / 10} m/s</li>
           </ul>
         </div>
       </div>

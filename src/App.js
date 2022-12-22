f97583e246bbc263457ca63978c68376b009c9a0
Hanna-Mik <110687941+Hanna-Mik@users.noreply.github.com>
2022-12-22 16:05:17 +0200

    added logo and header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Weather from "./Weather";
+import logo from "./images/logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="New York" />
+        <header>Weather App</header>
+        <Weather defaultCity="New York" />{" "}
+        <img src={logo} alt="Logo" className="logo" />
         <footer>
           This App was created by Hanna Shchepeleva and is{" "}
           <a
